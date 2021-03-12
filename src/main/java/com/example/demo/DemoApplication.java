282dc9625d2ab9d4227ed93b79f98670d2e107a5
Vincent <vncntduong@gmail.com>
2021-03-12 10:25:49 -0800

    testing branch added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World test";
+		return "Hello World testing";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
