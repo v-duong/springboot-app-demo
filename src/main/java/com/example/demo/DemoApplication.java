bcfaceacfc62bd6199341c8b4b70487d8626c884
Vincent Duong <v-duong@users.noreply.github.com>
2021-03-15 10:04:36 -0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World Banana 2";
+		return "Hello World Banana 2+1";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
